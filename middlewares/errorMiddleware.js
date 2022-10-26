b30b3781e9fbb59bb976b4367cd88e4545ce559a
Lucas <bithome1@outlook.com>
2022-10-26 20:18:08 -0300

    removido arquivos da trybe

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,7 +1,7 @@
 /* souerce: https://app.betrybe.com/course/back-end/introducao-ao-desenvolvimento-web-com-nodejs/express-middlewares/0ba5165f-5fda-4b6b-8de7-d2ccf5782c18/conteudos/d7afb381-bbae-497d-aea8-d41366f36066/pacote-express-rescue/aad27998-ad66-49f0-83f5-f1e927e5ec20?use_case=side_bar 
 
 Função retirada do course!
-O middleware de erro abaixo, verifica se o erro possui um código e um status HTTP. Possuindo, são devolvidos na resposta da requisição. Caso contrário, um erro genérico de servidor é delvolvido.
+O middleware de erro abaixo, verifica se o erro possui um código e um status HTTP. Possuindo, são devolvidos na resposta da requisição. Caso contrário, um erro genérico de servidor é devolvido.
 */
 const errorMiddleware = (err, _req, res, _next) => {
   if (err.code && err.status) {
